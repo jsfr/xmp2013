4755eccd9fe9088bf410faab3161c40cbddbdadd
jsfr <jfrdev@gmail.com>
2013-12-12 16:45:35 +0100

    Fixed problem with too many balls, and added project files

--- assignment1/src/machine.go
+++ assignment1/src/machine.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 )
 
 type binMsg struct {
@@ -28,16 +27,23 @@ func pin(in0 chan int, in1 chan int, out0 chan int, out1 chan int) {
 		case _, c0 := <-in0:
 			if !c0 {
 				in0 = nil
+			} else {
+				if choice == 0 {
+					out0 <- 1
+				} else {
+					out1 <- 1
+				}
 			}
 		case _, c1 := <-in1:
 			if !c1 {
 				in1 = nil
+			} else {
+				if choice == 0 {
+					out0 <- 1
+				} else {
+					out1 <- 1
+				}
 			}
-		}
-		if choice == 0 {
-			out0 <- 1
-		} else {
-			out1 <- 1
 		}
 	}
 	if out0 != out1 {
@@ -53,13 +59,16 @@ func bin(in0 chan int, in1 chan int, n int, message chan binMsg) {
 		case _, c0 := <-in0:
 			if !c0 {
 				in0 = nil
+			} else {
+				balls++
 			}
 		case _, c1 := <-in1:
 			if !c1 {
 				in1 = nil
+			} else {
+				balls++
 			}
 		}
-		balls++
 	}
 	message <- binMsg{n, balls}
 }
@@ -71,15 +80,15 @@ func twoLayerMachine(balls int, messages chan binMsg) {
 		chs[i] = make(chan int)
 	}
 
-	go pin(chs[0], chs[0], chs[1], chs[2])
+	go pin(chs[0], nil, chs[1], chs[2])
 	go pin(chs[1], chs[1], chs[3], chs[4])
 	go pin(chs[2], chs[2], chs[5], chs[6])
 	go pin(chs[3], chs[3], chs[7], chs[7])
 	go pin(chs[4], chs[5], chs[8], chs[8])
 	go pin(chs[6], chs[6], chs[9], chs[9])
-	go bin(chs[7], chs[7], 0, messages)
-	go bin(chs[8], chs[8], 1, messages)
-	go bin(chs[9], chs[9], 2, messages)
+	go bin(chs[7], nil, 0, messages)
+	go bin(chs[8], nil, 1, messages)
+	go bin(chs[9], nil, 2, messages)
 	go injector(chs[0], balls)
 }
 
@@ -87,17 +96,17 @@ func nLayerMachine(balls int, layers int, messages chan binMsg) {
 	chs := make([][](chan int), layers+2)
 	chs[0] = make([](chan int), 2)
 	chs[0][0] = make(chan int)
-	chs[0][1] = chs[0][0]
+	chs[0][1] = nil
 
-	for i := 0; i < layers+1; i++ {
-		chs[i+1] = make([](chan int), 2*i+4)
-		chs[i+1][0] = nil
-		chs[i+1][2*i+3] = nil
-		for j := 1; j < 2*i+3; j++ {
-			chs[i+1][j] = make(chan int)
+	for i := 1; i < layers+2; i++ {
+		chs[i] = make([](chan int), 2*i+2)
+		chs[i][0] = nil
+		chs[i][2*i+1] = nil
+		for j := 1; j < 2*i+1; j++ {
+			chs[i][j] = make(chan int)
 		}
-		for j := 0; j < i+1; j++ {
-			go pin(chs[i][2*j], chs[i][2*j+1], chs[i+1][2*j+1], chs[i+1][2*j+2])
+		for j := 0; j < i; j++ {
+			go pin(chs[i-1][2*j], chs[i-1][2*j+1], chs[i][2*j+1], chs[i][2*j+2])
 		}
 	}
 	for i := 0; i < layers+1; i++ {
@@ -107,22 +116,34 @@ func nLayerMachine(balls int, layers int, messages chan binMsg) {
 }
 
 func main() {
-	/*	go twoLayerMachine(1000, messages)
-		for n := 0; n < 3; n++ {
-			msg := <-messages
-			fmt.Println(msg)
-		}*/
+	var messages chan binMsg = make(chan binMsg)
+	var msgList []int
+	var layers int
+	var balls int = 1000
 
-	n, iter := 10, 1000
-	messages := make(chan binMsg)
-	msgList := make([]int, n+1)
-	go nLayerMachine(iter, n, messages)
-	for i := 0; i < n+1; i++ {
+	// Problem 1: Fixed Two-Layer Machine Simulation
+	msgList = make([]int, 3)
+	go twoLayerMachine(balls, messages)
+	for n := 0; n < 3; n++ {
 		msg := <-messages
 		msgList[msg.idx] = msg.balls
 	}
-	fmt.Println(n, " Layer Machine:")
 	for i, b := range msgList {
-		fmt.Println("Bin: ", strconv.Itoa(i), " Balls:", strconv.Itoa(b))
+		fmt.Println("Bin:", i, "Balls:", b)
 	}
+
+	// Problem 2: N-Layer Machine Simulation (set to 10 layers)
+	layers = 10
+	msgList = make([]int, layers+1)
+	go nLayerMachine(balls, layers, messages)
+	for i := 0; i < layers+1; i++ {
+		msg := <-messages
+		msgList[msg.idx] = msg.balls
+	}
+	fmt.Println(layers, "Layer Machine:")
+	for i, b := range msgList {
+		fmt.Println("Bin:", i, " Balls:", b)
+	}
+
+	// Problem 3: Oscilator
 }
