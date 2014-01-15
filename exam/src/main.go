bcbebf83f74f8b5b190c22c89aab9832a880897a
jsfr <jfrdev@gmail.com>
2014-01-15 22:18:18 +0100

    programming/theory part 1 almost done

--- exam/src/main.go
+++ exam/src/main.go
@@ -29,7 +29,7 @@ type Print struct {
 	Coords chan Coordinate
 }
 
-func Cell(in chan Msg, out map[int](chan Msg), coord Coordinate) {
+func Cell(in chan Msg, out [](chan Msg), coord Coordinate) {
 	pedestrian := false
 	dirc := -1
 	newMsg := false
@@ -76,7 +76,7 @@ func Cell(in chan Msg, out map[int](chan Msg), coord Coordinate) {
 }
 
 func Pedestrian(id string, dirc int, coord Coordinate, cell chan Msg,
-	clock chan Tick, tick chan int) {
+	clock chan Tick, tick chan int, printer chan Coordinate) {
 	in := make(chan Msg)
 	exit := false
 	for _ = range tick {
@@ -85,8 +85,8 @@ func Pedestrian(id string, dirc int, coord Coordinate, cell chan Msg,
 		r := rand.Intn(2)
 		d := make([]int, 3)
 		d[0] = dirc
-		d[2-r] = (dirc + 1) % 4
-		d[1+r] = (dirc - 1) % 4
+		d[2-r] = (dirc + 3) % 4
+		d[1+r] = (dirc + 5) % 4
 		for !done && tries <= 2 {
 			cell <- Msg{id, d[tries], in, true, coord}
 			msg := <-in
@@ -106,22 +106,50 @@ func Pedestrian(id string, dirc int, coord Coordinate, cell chan Msg,
 			}
 		}
 		if !exit {
-			fmt.Println("id:", id, "coord:", coord)
+			printer <- coord
 			clock <- Tick{id, tick}
 		} else {
 			clock <- Tick{id, nil}
 			close(tick)
 		}
 	}
+	close(printer)
 }
 
-func Injector(id string, clock chan Tick, tick chan int, dirc int, cell chan Msg) {
+func Printer(id string, in chan Print, clock chan Tick, tick chan int) {
+	pedestrians := make(map[string](chan Coordinate))
+	k := 0
+	for {
+		select {
+		case <-tick:
+			fmt.Println("######", "step", k, "######")
+			for i, c := range pedestrians {
+				coord, ok := <-c
+				if ok {
+					fmt.Println("id:", i, "coords:", coord)
+				} else {
+					fmt.Println("id", i, "walked of board")
+					delete(pedestrians, i)
+				}
+			}
+			clock <- Tick{id, tick}
+			k++
+		case p := <-in:
+			pedestrians[p.Id] = p.Coords
+		}
+	}
+}
+
+func Injector(id string, clock chan Tick, tick chan int, dirc int, cell chan Msg,
+	stdout chan Print) {
 	step := 0
 	for _ = range tick {
 		if step%4 == 0 {
 			tick := make(chan int)
+			printer := make(chan Coordinate)
 			clock <- Tick{id + strconv.Itoa(step/4), tick}
-			go Pedestrian(id+strconv.Itoa(step/4), dirc, Coordinate{-1, -1}, cell, clock, tick)
+			stdout <- Print{id + strconv.Itoa(step/4), printer}
+			go Pedestrian(id+strconv.Itoa(step/4), dirc, Coordinate{-1, -1}, cell, clock, tick, printer)
 		}
 		clock <- Tick{id, tick}
 		step++
@@ -148,31 +176,43 @@ func Clock(in chan Tick, clocks map[string](chan int), steps int) {
 			}
 		}
 	}
+	for _, c := range clocks {
+		close(c)
+	}
 }
 
 func Sidewalk() {
-	in := make([](chan Msg), 9)
+	x := 5
+	y := 10
+	cell := make([]([](chan Msg)), x+2)
+	cell[0] = make([](chan Msg), y+2)
+	cell[x+1] = make([]chan Msg, y+2)
+	for i := 1; i < x+1; i++ {
+		cell[i] = make([](chan Msg), y+2)
+		for j := 1; j < y+1; j++ {
+			cell[i][j] = make(chan Msg)
+		}
+	}
+	for i := 1; i < x+1; i++ {
+		for j := 1; j < y+1; j++ {
+			out := [](chan Msg){cell[i][j+1], cell[i+1][j], cell[i][j-1], cell[i-1][j]}
+			go Cell(cell[i][j], out, Coordinate{i, j})
+		}
+	}
+	tick := make([](chan int), 3)
+	for i := 0; i < 3; i++ {
+		tick[i] = make(chan int)
+	}
 	clock := make(chan Tick)
 	clocks := make(map[string](chan int))
-	tick0 := make(chan int)
-	tick1 := make(chan int)
-	clocks["A"] = tick0
-	clocks["B"] = tick1
-	for i := 0; i < 9; i++ {
-		in[i] = make(chan Msg)
-	}
-	go Cell(in[0], map[int](chan Msg){0: in[3], 1: in[1], 2: nil, 3: nil}, Coordinate{1, 1})
-	go Cell(in[1], map[int](chan Msg){0: in[4], 1: in[2], 2: nil, 3: in[0]}, Coordinate{2, 1})
-	go Cell(in[2], map[int](chan Msg){0: in[5], 1: nil, 2: nil, 3: in[1]}, Coordinate{3, 1})
-	go Cell(in[3], map[int](chan Msg){0: in[6], 1: in[4], 2: in[0], 3: nil}, Coordinate{1, 2})
-	go Cell(in[4], map[int](chan Msg){0: in[7], 1: in[5], 2: in[1], 3: in[3]}, Coordinate{2, 2})
-	go Cell(in[5], map[int](chan Msg){0: in[8], 1: nil, 2: in[2], 3: in[4]}, Coordinate{3, 2})
-	go Cell(in[6], map[int](chan Msg){0: nil, 1: in[7], 2: in[3], 3: nil}, Coordinate{1, 3})
-	go Cell(in[7], map[int](chan Msg){0: nil, 1: in[8], 2: in[4], 3: in[6]}, Coordinate{2, 3})
-	go Cell(in[8], map[int](chan Msg){0: nil, 1: nil, 2: in[5], 3: in[7]}, Coordinate{3, 3})
-	go Injector("A", clock, tick0, 0, in[0])
-	go Injector("B", clock, tick1, 0, in[1])
-	Clock(clock, clocks, 10)
+	stdout := make(chan Print)
+	clocks["A"] = tick[0]
+	clocks["B"] = tick[1]
+	clocks["Printer"] = tick[2]
+	go Printer("Printer", stdout, clock, tick[2])
+	go Injector("A", clock, tick[0], 0, cell[4][1], stdout)
+	go Injector("B", clock, tick[1], 1, cell[1][4], stdout)
+	Clock(clock, clocks, 20)
 }
 
 func main() {
